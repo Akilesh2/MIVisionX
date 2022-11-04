ed339c432248492c7cb5e9be8233b5feb9b2c75c
Akilesh2 <akilesh.giribala@multicorewareinc.com>
2022-11-04 05:08:26 +0000

    minor change on time calculation

--- utilities/audio_performance_test_akilesh/torch_audio.py
+++ utilities/audio_performance_test_akilesh/torch_audio.py
@@ -7,24 +7,22 @@ from torch.optim import Optimizer
 import time
 import os
 import timeit
+from tqdm import tqdm
 #change the folder_path 
-folder_path1 = '/media/audio/rpp/utilities/rpp-unittests/HOST_NEW/audio/../../../TEST_AUDIO_FILES/eight_samples_single_channel_src1/'
+folder_path1 = '/media/akilesh/audio/sample_audio/'
 
 file_list = os.listdir(folder_path1)
 tot_time = 0
-for i in range(100):
+for i in tqdm(range(100)):
     for f in file_list:
         filename = folder_path1+f
         waveform,sample_rate = torchaudio.load(filename)
-        spectro = torchaudio.transforms.Spectrogram(n_fft=512,win_length=512,center =True,power=2)
-        # MelSpectro = torchaudio.transforms.MelSpectrogram(sample_rate)
-        todecible = torchaudio.transforms.AmplitudeToDB(stype="amplitude", top_db=80)
-        switcher={
-            1:spectro(waveform),
-            # 2:MelSpectro(waveform),
-            3:todecible(waveform)
-        }
         start = timeit. default_timer()
-        switcher[3]
+        # spectro = torchaudio.transforms.Spectrogram(n_fft=512,win_length=512,center =True,power=2)
+        todecible = torchaudio.transforms.AmplitudeToDB(stype="amplitude", top_db=80)
+    
+        # spectro(waveform)
+        todecible(waveform)
         tot_time = tot_time + (timeit. default_timer()-start)
-print((tot_time/100) * 1000000)
+        
+print("Time in microseconds ",(tot_time/100) * 1000000)
--- utilities/audio_performance_test_akilesh/torch_image.py
+++ utilities/audio_performance_test_akilesh/torch_image.py
@@ -4,21 +4,25 @@ from PIL import Image
 import cv2
 import os
 import timeit
+import torchvision.transforms.functional as F
+from tqdm import tqdm
+
 
 tot_time =0 
-for i in range(100):
-    folder_path = "/media/audio_samples/MIVisionX-data/rocal_data/100sample/"
-    file_list = os.listdir(folder_path)
-    
+for i in tqdm(range(100)):
+    folder_path = "/media/akilesh/100_sample_image/"
+    file_list = os.listdir(folder_path)  
     for fn in file_list:
         file_name = folder_path + fn 
         img = Image.open(file_name)
         if(not img):
             print("Error in image read ",file_name)
             exit(0)
-        center_crop = torchvision.transforms.ColorJitter( brightness = 1.4 , contrast = 0.4 , saturation = 1.9, hue = 0.1 )
         start = timeit. default_timer()
-        img = center_crop(img)
+        # color_jitter = torchvision.transforms.ColorJitter( brightness = 1.4 , contrast = 0.4 , saturation = 1.9, hue = 0.1 )
+        # bright = F.adjust_brightness(img, 0.3)
+        contrast = F.adjust_contrast(img,0.1)
+        # img = color_jitter(img)
         tot_time = tot_time + (timeit. default_timer()-start)
-print(tot_time*10000)  
+print("Time in microseconds ",(tot_time/100)*1000000)  
 
